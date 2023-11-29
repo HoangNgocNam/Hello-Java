bae4322baa0dd1941d52d4e5daf32dbd644b69b5
HoangNgocNam <hoangngocnam2310@gmail.com>
2023-11-29 14:55:48 +0700

    NhanHaiSo

---
+++ NhanHaiSo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NhanHaiSo {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+    //     System.out.println("Nhập số thứ nhất:");
+    //     int num1 = scanner.nextInt();
+    //     System.out.println("Nhập số thứ hai:");
+    //     int num2 = scanner.nextInt();
+          
+
+    //     int tích = num1*num2;
+
+    //     System.out.println("Tích Hai Số Là:" +tích);
+
+        System.out.println("Nhập số thứ nhất:");
+
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Nhập số thứ hai:");
+
+        double num2 = scanner.nextDouble();
+
+        double tich = num1*num2;
+
+        System.out.println("Kết quả:" +tich);
+
+     }
+}
+
+
+
